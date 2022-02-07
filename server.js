2d723f55167711f43a0ff5a3b7b252b3fd5f7865
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-02-07 14:32:47 +0700

    chore: setup prettier

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ const { Server } = require('socket.io');
 const app = express();
 const httpServer = createServer(app);
 httpServer.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log('Server is running on port 5000');
+});
+
+const io = new Server(httpServer, {
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+  cors: {
+    origin: '*',
+  },
 });
