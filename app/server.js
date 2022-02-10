d287d8a652e7799e61af07509de8b59a6de7eb8f
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-02-10 21:49:13 +0700

    refactor: put server init outside app layer

--- app/server.js
+++ app/server.js
@@ -1,31 +1,14 @@
-const { createServer } = require('http');
 const express = require('express');
-const { Server } = require('socket.io');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const app = express();
-const httpServer = createServer(app);
-httpServer.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
 
-const io = new Server(httpServer, {
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  cookie: false,
-  cors: {
-    origin: '*',
-  },
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-io.on('connection', (socket) => {
-  console.log('User connected');
-
-  socket.on('chat', (data) => {
-    console.log(data);
-    io.emit('chat', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+module.exports = app;
