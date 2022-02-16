affa0b1be9b754c24884e7a282f6729559c16fa4
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-02-16 11:42:13 +0700

    feat: add bcrypt helpers

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,10 +1,9 @@
-// const { isEmpty, response, hashPassword } = require('../helpers/bcrypt');
+const { checkPassword, hashPassword } = require('../helpers/bcrypt');
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
 const { User } = require('../../database/models');
-// const ResetPassword = require('../models/ResetPassword');
 // const { generateAccessToken } = require('../helpers/jwt');
-const { DuplicatedDataError } = require('../exceptions');
+const { DuplicatedDataError, NotFoundError } = require('../exceptions');
 
 module.exports = {
   register: async (req, res) => {
@@ -19,7 +18,7 @@ module.exports = {
 
       const user = await User.create({
         username,
-        password,
+        password: await hashPassword(password),
       });
 
       return res.status(201).json({
@@ -42,79 +41,78 @@ module.exports = {
 
   },
   login: async (req, res) => {
-    const { username, password } = req.body;
-    let token;
-    let client;
+    //     const { username, password } = req.body;
+    //     let token;
+    //     let client;
 
-    // try {
-    //   let user = await User.findOne({ email });
+    //     try {
+    //       let user = await User.findOne({ email });
 
-    //   if (!isEmpty(user)) {
-    //     if (!user.verifiedAt)
-    //       throw new WrongIdentityError('Please verify your email first!');
-    //     const checkPassword = await bcrypt.compare(password, user.password);
-    //     if (!checkPassword)
-    //       throw new WrongPasswordError(
-    //         "Username or password doesn't match our records!"
-    //       );
-    //     // prevent password to be shown in response
-    //     user.password = undefined;
-    //     user = JSON.parse(JSON.stringify(user));
+    //       if (!user) throw new NotFoundError('User not found');
 
-    //     const mitra = await Mitra.findOne({ user });
-    //     const customer = await Customer.findOne({ user });
+    //       if (!user.verifiedAt)
+    //         throw new WrongIdentityError('Please verify your email first!');
+    //       const checkPassword = await bcrypt.compare(password, user.password);
+    //       if (!checkPassword)
+    //         throw new WrongPasswordError(
+    //           "Username or password doesn't match our records!"
+    //         );
+    //       // prevent password to be shown in response
+    //       user.password = undefined;
+    //       user = JSON.parse(JSON.stringify(user));
 
-    //     // create new jwt token
-    //     if (!isEmpty(mitra)) {
-    //       token = generateAccessToken({
-    //         id: user._id,
-    //         idMitra: mitra._id,
-    //         email: user.email,
-    //         role: user.role,
-    //       });
-    //       client = { user, mitra };
-    //     } else if (!isEmpty(customer)) {
-    //       token = generateAccessToken({
-    //         id: user._id,
-    //         idCustomer: customer._id,
-    //         email: user.email,
-    //         role: user.role,
-    //       });
-    //       client = { user, customer };
-    //     } else {
-    //       throw new NotFoundError("User doesn't exists!");
-    //     }
-    //   } else if (!isEmpty(admin)) {
-    //     const checkAdminPassword = await bcrypt.compare(
-    //       password,
-    //       admin.password
+    //       const mitra = await Mitra.findOne({ user });
+    //       const customer = await Customer.findOne({ user });
+
+    //       // create new jwt token
+    //       if (!isEmpty(mitra)) {
+    //         token = generateAccessToken({
+    //           id: user._id,
+    //           idMitra: mitra._id,
+    //           email: user.email,
+    //           role: user.role,
+    //         });
+    //         client = { user, mitra };
+    //       } else if (!isEmpty(customer)) {
+    //         token = generateAccessToken({
+    //           id: user._id,
+    //           idCustomer: customer._id,
+    //           email: user.email,
+    //           role: user.role,
+    //         });
+    //         client = { user, customer };
+    //       } else {
+    //         throw new NotFoundError("User doesn't exists!");
+    //       }
+    //     } else if(!isEmpty(admin)) {
+    //   const checkAdminPassword = await bcrypt.compare(
+    //     password,
+    //     admin.password
+    //   );
+    //   if (!checkAdminPassword)
+    //     throw new WrongPasswordError(
+    //       'Your password does not match with our records!'
     //     );
-    //     if (!checkAdminPassword)
-    //       throw new WrongPasswordError(
-    //         'Your password does not match with our records!'
-    //       );
-    //     // create new jwt token
-    //     token = generateAccessToken({
-    //       idAdmin: admin._id,
-    //       email: admin.email,
-    //       role: 'admin',
-    //     });
-    //     admin.role = 'admin';
-    //     // prevent password to be shown in response
-    //     admin.password = undefined;
-    //     admin = JSON.parse(JSON.stringify(admin));
-    //     client = { admin };
-    //   } else throw new NotFoundError("Username or password doesn't match our records!");
-
-    //   // store the token in user browser cookie
-    //   res.cookie('token', token, { httpOnly: true });
-    //   return response(res, {
-    //     code: 200,
-    //     success: true,
-    //     message: 'Login successfully!',
-    //     content: { client, token },
+    //   // create new jwt token
+    //   token = generateAccessToken({
+    //     idAdmin: admin._id,
+    //     email: admin.email,
+    //     role: 'admin',
     //   });
-    // } catch (error) {
+    //   admin.role = 'admin';
+    //   // prevent password to be shown in response
+    //   admin.password = undefined;
+    //   admin = JSON.parse(JSON.stringify(admin));
+    //   client = { admin };
+    // } else throw new NotFoundError("Username or password doesn't match our records!");
+
+    // return response(res, {
+    //   code: 200,
+    //   success: true,
+    //   message: 'Login successfully!',
+    //   content: { client, token },
+    // });
+    //     } catch (error) {
     //   if (
     //     error.name === 'NotFoundError' ||
     //     error.name === 'WrongPasswordError' ||
