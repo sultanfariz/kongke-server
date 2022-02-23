ead6fe07b333e21bbfb1074eea3904c3a88d72a3
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-02-23 08:43:11 +0700

    feat: split chat by chatroom

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,5 +1,6 @@
 const { Server } = require('socket.io');
 const socketioJwt = require('socketio-jwt');
+const { parseJwtPayload } = require('../helpers/jwt');
 
 function loadSocket(httpServer) {
   const io = new Server(httpServer, {
@@ -21,20 +22,38 @@ function loadSocket(httpServer) {
 
     socket.on('join', (data) => {
       socket.join(data.room);
-      console.log(`User ${data.username} joined room ${data.room}`);
-      io.emit('chat', {
-        user: "SYSTEM",
-        text: `User ${data.username} joined room ${data.room}`,
-        date: new Date().valueOf(),
-      });
+      console.log(`User ${data.user} joined room ${data.room}`);
+      io.to(data.room)
+        .emit('chat', {
+          user: "SYSTEM",
+          text: `User ${data.user} joined room ${data.room}`,
+          date: new Date().valueOf(),
+        });
     });
 
     socket.on('chat', (data) => {
       console.log(data);
       if (data?.user && data?.text)
-        io.emit('chat', {
-          user: data.user,
-          text: data.text,
+        io.to(data.room)
+          .emit('chat', {
+            user: data.user,
+            text: data.text,
+            date: new Date().valueOf(),
+          });
+    });
+
+    socket.on("disconnecting", () => {
+      console.log(socket.rooms); // the Set contains at least the socket ID
+      const rooms = Array.from(socket.rooms);
+      const token = socket.handshake.query.token;
+      const parsed = parseJwtPayload(token);
+      const { username } = parseJwtPayload(token);
+      console.log("parsed", parsed);
+      console.log("username", username);
+      io.to(rooms[1])
+        .emit('chat', {
+          user: "SYSTEM",
+          text: `User ${username} left room ${rooms[1]}`,
           date: new Date().valueOf(),
         });
     });
